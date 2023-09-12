b1245272ebc30b2484e874c0a6045973ac503ee4
Neis Rosado <neisrosadom@gmail.com>
2023-09-12 16:02:04 -0500

    Add category filter in home

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
 import authReducer from './auth/authReducer';
+import productsReducer from './products/productsReducer';
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    products: productsReducer, 
     },
   middleware: [thunk]
 });
