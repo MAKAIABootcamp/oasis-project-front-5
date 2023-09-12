d34fa9fabe77809a434b67d7f53012e367c05dbc
ximena saldarriaga <xime-2208@hotmail.com>
2023-09-12 10:36:32 -0500

    add styles to personalData

--- src/pages/personalData/PersonalData.jsx
+++ src/pages/personalData/PersonalData.jsx
@@ -21,39 +21,43 @@ const PersonalData = () => {
                         <p>Nombre:</p>
                         <p> Camila Sánchez</p>
                     </div>
-                    <img className='w-4 object-contain' src={edit} alt="" />
+                    <img className='w-4 object-contain cursor-pointer' src={edit} alt="" />
                 </div>
+                <hr />
 
                 <div className='flex justify-between w-60'>
                     <div className='flex gap-2'>
                         <p>Documento:</p>
                         <p> 985414788</p>
                     </div>
-                    <img className='w-4 object-contain' src={edit} alt="" />
+                    <img className='w-4 object-contain cursor-pointer' src={edit} alt="" />
                 </div>
+                <hr />
 
                 <div className='flex justify-between w-60'>
                     <div className='flex gap-2'>
                         <p>Correo:</p>
                         <p>camila@gmail.com</p>
                     </div>
-                    <img className='w-4 object-contain' src={edit} alt="" />
+                    <img className='w-4 object-contain cursor-pointer' src={edit} alt="" />
                 </div>
+                <hr />
 
                 <div className='flex justify-between w-60'>
                     <div className='flex gap-2'>
                         <p>Celular:</p>
                         <p>3002525478</p>
                     </div>
-                    <img className='w-4 object-contain' src={edit} alt="" />
+                    <img className='w-4 object-contain cursor-pointer' src={edit} alt="" />
                 </div>
+                <hr />
 
                 <div className='flex justify-between w-60'>
                     <div className='flex gap-2'>
                         <p>Dirección:</p>
                         <p> Cra 58 # 21-25 Medellín, Antioquia</p>
                     </div>
-                    <img className='w-4 object-contain' src={edit} alt="" />
+                    <img className='w-4 object-contain cursor-pointer' src={edit} alt="" />
                 </div>
 
 
