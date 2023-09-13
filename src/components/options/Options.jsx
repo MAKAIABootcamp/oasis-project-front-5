2c9bb506b8ecbb7ac4301193c4e858a37690fef5
ximena saldarriaga <xime-2208@hotmail.com>
2023-09-13 15:15:22 -0500

    add responsive to options and header

--- src/components/options/Options.jsx
+++ src/components/options/Options.jsx
@@ -3,14 +3,15 @@ import { useNavigate } from 'react-router-dom'
 import comunity from '../../assets/comunity.png'
 import home from '../../assets/home.png'
 import user from '../../assets/user.png'
+import './options.scss'
 
 const Options = () => {
     const navigate = useNavigate();
     return (
         <div className='flex gap-4'>
-            <img className='w-5 object-contain cursor-pointer' onClick={() => navigate('/')} src={home} alt="" />
-            <img className='w-5 object-contain cursor-pointer' onClick={() => navigate('/login')} src={user} alt="" />
-            <img className='w-7 object-contain cursor-pointer' onClick={() => navigate('/blog')} src={comunity} alt="" />
+            <img className='options' onClick={() => navigate('/')} src={home} alt="" />
+            <img className='options' onClick={() => navigate('/login')} src={user} alt="" />
+            <img className='options comunity' onClick={() => navigate('/blog')} src={comunity} alt="" />
         </div>
     )
 }
