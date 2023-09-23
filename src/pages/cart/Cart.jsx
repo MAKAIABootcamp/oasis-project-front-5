646e810a969144c5387c37af112d42b3cda4592e
ximena saldarriaga <xime-2208@hotmail.com>
2023-09-23 17:36:55 -0500

    create cartSlice

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,50 +1,71 @@
-import React from 'react'
-import del from '../../assets/delete.png'
-import blue from '../../assets/blue.jpg'
-import { useNavigate } from 'react-router';
-import './cart.scss'
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { removeFromCart } from '../../redux/store/cart/cartSlice';
+import del from '../../assets/delete.png';
+import './cart.scss';
 import Header from '../../components/header/Header';
-import Sidebar from "../../components/sidebar/Sidebar";
-import Paragraph from '../../components/paragraph/Paragraph';
+import Sidebar from '../../components/sidebar/Sidebar';
 
 const Cart = () => {
-    const navigate = useNavigate();
-    return (
-        <>
-            <Header />
-            <div className='cart flex flex-col'>
-                <h1 className='title self-center'>CARRITO</h1>
-                <div className='cart__container'>
-                    <Sidebar />
-                    <Paragraph />
-                    <div className='flex gap-8'>
-                        <img className='w-40' src={blue} alt="" />
-                        <div className='flex flex-col gap-14'>
-                            <div className='flex flex-col gap-4'>
-                                <div className='flex justify-between'>
-                                    <p className='font-semibold'>$ 15.000</p>
-                                    <img className='w-5 object-contain cursor-pointer' src={del} alt="" />
-                                </div>
-                                <p className='font-semibold'>Blusa blanca de tela suave</p>
-                                <p>Talla M</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className='flex flex-col gap-8'>
+  const dispatch = useDispatch();
+  const cartItems = useSelector((state) => state.cart.cartItems);
+  const handleRemoveFromCart = (productId) => {
+    dispatch(removeFromCart({ id: productId }));
+  };
+  const total = cartItems.reduce((acc, item) => acc + item.price, 0);
 
-                        <div className='flex justify-between'>
-                            <div className='flex gap-2'>
-                                <p className='font-semibold'>Total</p>
-                                <p>(+ envío)</p>
-                            </div>
-                            <p className='font-semibold'>$ 15.000</p>
-                        </div>
-                        <button className="button__page px-6 py-1.5 w-[100%]" onClick={() => navigate('/location')}>Continuar con la compra</button>
+  return (
+    <>
+      <Header />
+      <div className="cart flex flex-col">
+        <h1 className="title self-center">CARRITO</h1>
+        <div className="cart__container">
+          <Sidebar />
+          {cartItems.length === 0 ? (
+            <p>Tu carrito está vacío</p>
+          ) : (
+            <div>
+              {cartItems.map((item) => (
+                <div key={item.id} className="flex gap-8">
+                  <img className="w-40" src={item.gallery.frontPage} alt={item.name} />
+                  <div className="flex flex-col gap-14">
+                    <div className="flex flex-col gap-4">
+                      <div className="flex justify-between">
+                        <p className="font-semibold">$ {item.price}</p>
+                        <button
+                          onClick={() => handleRemoveFromCart(item.id)}
+                          className="w-5 object-contain cursor-pointer"
+                        >
+                          <img src={del} alt="Remove" />
+                        </button>
+                      </div>
+                      <p className="font-semibold">{item.name}</p>
+                      <p>Talla {item.size}</p>
                     </div>
+                  </div>
                 </div>
+              ))}
+              <div className="flex flex-col gap-8">
+                <div className="flex justify-between">
+                  <div className="flex gap-2">
+                    <p className="font-semibold">Total</p>
+                    <p>(+ envío)</p>
+                  </div>
+                  <p className="font-semibold">$ {total}</p>
+                </div>
+                <button
+                  className="button__page px-6 py-1.5 w-[100%]"
+                  onClick={() => navigate('/location')}
+                >
+                  Continuar con la compra
+                </button>
+              </div>
             </div>
-        </>
-    )
-}
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Cart
+export default Cart;
