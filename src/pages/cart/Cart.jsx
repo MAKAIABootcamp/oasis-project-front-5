4314a5ef54ca776b5aad154bd23dbbb5e71d6849
ximena saldarriaga <xime-2208@hotmail.com>
2023-09-25 21:06:15 -0500

    add styles

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { removeFromCart } from '../../redux/store/cart/cartSlice';
 import del from '../../assets/delete.png';
@@ -13,6 +13,8 @@ const Cart = () => {
 
     const [cartProducts, setCartProducts] = useState([]);
     const user = auth.currentUser;
+    const dispatch = useDispatch();
+    const cartItems = useSelector((state) => state.cart.cartItems);
 
     useEffect(() => {
         const fetchCartProducts = async () => {
@@ -31,69 +33,67 @@ const Cart = () => {
         fetchCartProducts();
     }, [user]);
 
-  const dispatch = useDispatch();
-  const cartItems = useSelector((state) => state.cart.cartItems);
-  const handleRemoveFromCart = (productId) => {
-    dispatch(removeFromCart({ id: productId }));
-  };
-  const total = cartItems.reduce((acc, item) => acc + item.price, 0);
 
-  return (
-    <>
-      <Header />
-      <div className="cart flex flex-col">
-        <h1 className="title self-center">CARRITO</h1>
-        <div className="cart__container">
-          <Sidebar />
-          <div className='cart__card'>
-          {cartProducts.length === 0 ? (
-            <p>Tu carrito está vacío</p>
-          ) : (
-            <div className='cart__item'>
-              {cartProducts.map((item) => (
-                <div key={item.id} className="cart__info">
-                    <Link to={`/details/${item.id}`} className="product-image-link">
-                  <img className="w-40" src={item.gallery.poster} alt={item.name} />
-                  </Link>
-                  <div className="flex flex-col gap-14">
-                    <div className="flex flex-col gap-4">
-                      <div className="flex justify-between">
-                        <p className="font-semibold">$ {item.price}</p>
-                        <button
-                          onClick={() => handleRemoveFromCart(item.id)}
-                          className="w-5 object-contain cursor-pointer"
-                        >
-                          <img src={del} alt="Remove" />
-                        </button>
-                      </div>
-                      <p className="font-semibold">{item.name}</p>
-                      <p>Talla {item.size}</p>
+    const handleRemoveFromCart = (productId) => {
+        dispatch(removeFromCart({ id: productId }));
+        fetchCartProducts();
+    };
+    const total = cartItems.reduce((acc, item) => acc + item.price, 0);
+
+    return (
+        <>
+            <Header />
+            <div className="cart flex flex-col">
+                <h1 className="title self-center">CARRITO</h1>
+                <div className="cart__container">
+                    <Sidebar />
+                    <div className='cart__card'>
+                        {cartProducts.length === 0 ? (
+                            <p>Tu carrito está vacío</p>
+                        ) : (
+                            <div className='cart__item'>
+                                {cartProducts.map((item) => (
+                                    <div key={item.id} className="cart__info">
+                                        <Link to={`/details/${item.id}`} className="product-image-link">
+                                            <img className="w-40" src={item.gallery.poster} alt={item.name} />
+                                        </Link>
+                                        <div className="flex flex-col gap-14">
+                                            <div className="flex flex-col gap-4">
+                                                <p className="font-semibold">$ {item.price}</p>
+                                                <p className="font-semibold">{item.name}</p>
+                                                <p>Talla {item.size}</p>
+                                                <button
+                                                    onClick={() => handleRemoveFromCart(item.id)}
+                                                    className="w-5 object-contain cursor-pointer"
+                                                >
+                                                    <img src={del} alt="Remove" />
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
+                        )}
                     </div>
-                  </div>
+                    <div className="flex flex-col gap-8">
+                        <div className="flex justify-between">
+                            <div className="flex gap-2">
+                                <p className="font-semibold">Total</p>
+                                <p>(+ envío)</p>
+                            </div>
+                            <p className="font-semibold">$ {total}</p>
+                        </div>
+                        <button
+                            className="button__page px-6 py-1.5 w-[100%]"
+                            onClick={() => navigate('/location')}
+                        >
+                            Continuar con la compra
+                        </button>
+                    </div>
                 </div>
-              ))}
-              <div className="flex flex-col gap-8">
-                <div className="flex justify-between">
-                  <div className="flex gap-2">
-                    <p className="font-semibold">Total</p>
-                    <p>(+ envío)</p>
-                  </div>
-                  <p className="font-semibold">$ {total}</p>
-                </div>
-                <button
-                  className="button__page px-6 py-1.5 w-[100%]"
-                  onClick={() => navigate('/location')}
-                >
-                  Continuar con la compra
-                </button>
-              </div>
             </div>
-          )}
-          </div>
-        </div>
-      </div>
-    </>
-  );
+        </>
+    );
 };
 
 export default Cart;
