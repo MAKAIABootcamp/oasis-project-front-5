d443ec04f68d94e1bae7cf1131ed6580a7f30da2
ximena saldarriaga <xime-2208@hotmail.com>
2023-10-02 10:29:56 -0500

    add styles to orders

--- src/pages/orders/Orders.jsx
+++ src/pages/orders/Orders.jsx
@@ -2,15 +2,17 @@ import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { collection, query, getDocs } from 'firebase/firestore';
 import { fireStore, auth } from "../../firebase/firebaseConfig";
-import bag from "../../assets/bag.png";
+import empty from '../../assets/empty.png';
 import Header from '../../components/header/Header';
 import Sidebar from "../../components/sidebar/Sidebar";
+import { useNavigate} from 'react-router-dom';
 import './orders.scss'
 
 const Orders = () => {
 
     const userLogged = useSelector((state) => state.auth.userLogged);
     const [userOrders, setUserOrders] = useState([]);
+    const navigate = useNavigate();
 
     useEffect(() => {
         if (!userLogged) {
@@ -48,14 +50,14 @@ const Orders = () => {
             <Header showSearchBar={false} />
             <div className="favorites flex flex-col ">
                 <h1 className="font-semibold self-center"> MIS COMPRAS </h1>
-                <div className="favorites__container flex">
+                <div className="favorites__container  flex">
                     <Sidebar />
                     {userOrders.length === 0 ? (
-                        <div className='flex gap-5 items-center'>
-                            <img className="w-20 object-contain" src={bag} alt="" />
-                            <span className='text-[14px] text-gray-400'>
-                                Aún no ha realizado su primera compra.
-                            </span>
+                        <div className='favorites__info w-[100%] cart__infoEmpty flex flex-col items-center gap-4'>
+                            <img className='cart__empty flex self-center' src={empty} alt="" />
+                            <p className='font-bold text-center'>Aún no has realizado compras  </p>
+                            <p className='flex text-center'>¡Aprovecha! Tenemos miles de productos y oportunidades únicas.</p>
+                            <button className='button__page cart__button' onClick={() => navigate('/products')}>Ver productos</button>
                         </div>
                     ) : (
                         <div className="favorites__card">
