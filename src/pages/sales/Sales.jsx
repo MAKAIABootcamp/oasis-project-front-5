c7048957cca6346facde3eac1939deea8278dbbd
ximena saldarriaga <xime-2208@hotmail.com>
2023-10-03 16:16:43 -0500

    add styles to sales

--- src/pages/sales/Sales.jsx
+++ src/pages/sales/Sales.jsx
@@ -4,13 +4,14 @@ import Header from "../../components/header/Header";
 import Sidebar from "../../components/sidebar/Sidebar";
 import { collection, getDocs } from "firebase/firestore";
 import { fireStore } from "../../firebase/firebaseConfig";
-import { BarChart, Bar, PieChart, Pie } from "recharts";
 
 const Sales = () => {
   const [salesData, setSalesData] = useState([]);
   const [totalSales, setTotalSales] = useState(0);
+  const [totalSalesData, setTotalSalesData] = useState([{ total: 0 }]);
 
   useEffect(() => {
+
     const fetchSalesData = async () => {
       try {
         const salesCollection = collection(fireStore, "ventas");
@@ -19,15 +20,13 @@ const Sales = () => {
         console.log("Datos de ventas desde Firestore:", sales);
 
         const totalSalesAmount = sales.reduce((acc, sale) => {
-          if (typeof sale.orderData.cartData[0].price === "number") {
-            return acc + sale.orderData.cartData[0].price;
+          if (typeof sale.orderData.total === "number") {
+            return acc + sale.orderData.total;
           }
           return acc;
         }, 0);
 
-        console.log("Total de ventas:", totalSalesAmount);
-        console.log("Datos para las gráficas:", sales);
-
+        setTotalSalesData([{ total: totalSalesAmount }]);
         setSalesData(sales);
         setTotalSales(totalSalesAmount);
       } catch (error) {
@@ -45,61 +44,38 @@ const Sales = () => {
         <div className="products__container">
           <Sidebar />
           <div className="sales-container">
-            <br />
-            <div className="charts flex ">
-              <div className="chart">
-                <br />
-                <BarChart width={190} height={300} data={totalSales}>
-                  <Bar dataKey="total" fill="#8884d8" />
-                </BarChart>
-
-                <br />
-                <span className="text"> Ingreso Total: ${totalSales}</span>
-              </div>
-              <div className="chart">
-                <span className="text">Informe de Ventas</span>
-                <PieChart width={300} height={300}>
-                  <Pie
-                    dataKey="total"
-                    isAnimationActive={false}
-                    data={salesData.length}
-                    cx="50%"
-                    cy="50%"
-                    outerRadius={80}
-                    fill="#8884d8"
-                    label
-                  />
-                </PieChart>
-                <br />
-                <span className="text">
-                  Total de Ventas: {salesData.length}
-                </span>
-              </div>
+            <h2 className="fontGreen title flex self-center mb-8">HISTORIAL DE VENTAS</h2>
+            <div className="flex justify-between">
+            <p className="text"> Ingreso Total: ${totalSales}</p>
+              <p className="text">Total de Ventas: {salesData.length}</p>
             </div>
-            <div className="sales-history">
-              <h2>Historial de Ventas</h2>
-              <table>
-                <thead>
-                  <tr>
-                    <th>Fecha de Venta</th>
-                    <th>Nombre del Producto</th>
-                    <th>Precio</th>
+            <table className="sales-history">
+              <thead>
+                <tr>
+                  <th>Fecha de Venta</th>
+                  <th>Productos</th>
+                  <th>Valor de compra</th>
+                  <th>Dirección</th>
+                  <th>Nombre del cliente</th>
+                  <th>Correo</th>
+                </tr>
+              </thead>
+              <tbody>
+                {salesData.map((sale, index) => (
+                  <tr key={index}>
+                    <td>
+                      {" "}
+                      {new Date(sale.timestamp.toDate()).toLocaleString()}
+                    </td>
+                    <td>{sale.orderData.cartData[0].name}</td>
+                    <td>${sale.orderData.total}</td>
+                    <td>{sale.orderData.selectedAddress}</td>
+                    <td>{sale.orderData.nombre}</td>
+                    <td>{sale.orderData.correo}</td>
                   </tr>
-                </thead>
-                <tbody>
-                  {salesData.map((sale, index) => (
-                    <tr key={index}>
-                      <td>
-                        {" "}
-                        {new Date(sale.timestamp.toDate()).toLocaleString()}
-                      </td>
-                      <td>{sale.orderData.cartData[0].name}</td>
-                      <td>${sale.orderData.cartData[0].price}</td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
+                ))}
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
