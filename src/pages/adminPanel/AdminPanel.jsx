24381cf1db74418dca8edda8616b8ac86f8d71aa
ximena saldarriaga <xime-2208@hotmail.com>
2023-10-04 17:35:04 -0500

    add styles

--- src/pages/adminPanel/AdminPanel.jsx
+++ src/pages/adminPanel/AdminPanel.jsx
@@ -54,7 +54,7 @@ const AdminPanel = () => {
 
   useEffect(() => {
     if (localSelectedCategory) {
-      setIsLoading(true); 
+      setIsLoading(true);
       dispatch(fetchItems(localSelectedCategory))
         .then(() => {
           setIsLoading(false);
@@ -72,91 +72,85 @@ const AdminPanel = () => {
     <>
       <Header showSearchBar={false} />
       <div className="adminProduct">
-        <div className="adminProduct__all">
-          <Sidebar />
-          <div className="adminProduct__container">
-            {isLoading ? null : ( 
-              <h1 className="flex self-center fontGreen title"> PRODUCTOS </h1>
-            )}
-            <div className="product-list">
-              {isLoading ? (
-                <Loading />
-              ) : (
-                <>
-                  {selectedProfileView === "stock" && (
-                    <>
-                      <div className="adminProduct__list">
-                        {localSearchTerm && noResults ? (
-                          <div className="no-results-message">
-                            Lo siento, no se encontraron productos.
-                          </div>
-                        ) : (
-                          filteredProducts.map((product, index) => (
-                            <div
-                              className={`adminProduct__div flex gap-2 ${
-                                product.sold ? "sold" : ""
+        <Sidebar />
+        <div className="adminProduct__container">
+
+            {isLoading ? (
+              <Loading />
+            ) : (
+              <>
+                {selectedProfileView === "stock" && (
+                  <>
+                    <h1 className="fontGreen flex self-center title mb-10 adminProduct__title"> PRODUCTOS </h1>
+                    <div className="adminProduct__list">
+                      {localSearchTerm && noResults ? (
+                        <div className="no-results-message">
+                          Lo siento, no se encontraron productos.
+                        </div>
+                      ) : (
+                        filteredProducts.map((product, index) => (
+                          <div
+                            className={`adminProduct__div flex gap-2 ${product.sold ? "sold" : ""
                               }`}
-                              key={index}
-                            >
-                              {product.sold ? (
-                                <div className="adminProduct__item">
-                                  <img
-                                    className="w-[100px] h-[100px] object-cover rounded-md cursor-pointer relative "
-                                    src={product.poster}
-                                    alt={product.name}
-                                  />
-                                  <p className="fontGreen w-[150px]">
-                                    {product.title}
-                                  </p>
-                                  <span className="adminProduct__info w-[150px]">
-                                    {product.status}
-                                  </span>
-                                  <p className="adminProduct__info fontGreen w-[150px]">
-                                    ${product.price}
-                                  </p>
-                                  <p className="adminProduct__info w-[150px]">{product.genre}</p>
-                                  <p className="adminProduct__agotado font-semibold w-[150px] text-red-500">
-                                    AGOTADO
-                                  </p>
+                            key={index}
+                          >
+                            {product.sold ? (
+                              <div className="adminProduct__item">
+                                <img
+                                  className="w-[100px] h-[100px] object-cover rounded-md cursor-pointer relative "
+                                  src={product.poster}
+                                  alt={product.name}
+                                />
+                                <p className="fontGreen w-[150px]">
+                                  {product.title}
+                                </p>
+                                <span className="adminProduct__info w-[150px]">
+                                  {product.status}
+                                </span>
+                                <p className="adminProduct__info fontGreen w-[150px]">
+                                  ${product.price}
+                                </p>
+                                <p className="adminProduct__info w-[150px]">{product.genre}</p>
+                                <p className="adminProduct__agotado font-semibold w-[150px] text-red-500">
+                                  AGOTADO
+                                </p>
+                                <img className="adminProduct__edite" src={edite} alt="" />
+                              </div>
+                            ) : (
+                              <div className="adminProduct__item">
+                                <img
+                                  className="w-[100px] h-[100px] object-cover rounded-md cursor-pointer "
+                                  src={product.poster}
+                                  alt={product.name}
+                                />
+                                <p className="fontGreen w-[150px]">
+                                  {product.title}
+                                </p>
+                                <span className="adminProduct__info w-[150px]">
+                                  {product.status}
+                                </span>
+                                <p className="adminProduct__info fontGreen w-[150px]">
+                                  ${product.price}
+                                </p>
+                                <p className="w-[150px] adminProduct__info">{product.genre}</p>
+                                <p className="w-[150px] adminProduct__info">{product.state}</p>
+                                <Link
+                                  to={`/admin-details/${product.id}`}
+                                >
                                   <img className="adminProduct__edite" src={edite} alt="" />
-                                </div>
-                              ) : (
-                                <div className="adminProduct__item">
-                                  <img
-                                    className="w-[100px] h-[100px] object-cover rounded-md cursor-pointer "
-                                    src={product.poster}
-                                    alt={product.name}
-                                  />
-                                  <p className="fontGreen w-[150px]">
-                                    {product.title}
-                                  </p>
-                                  <span className="adminProduct__info w-[150px]">
-                                    {product.status}
-                                  </span>
-                                  <p className="adminProduct__info fontGreen w-[150px]">
-                                    ${product.price}
-                                  </p>
-                                  <p className="w-[150px] adminProduct__info">{product.genre}</p>
-                                  <p className="w-[150px] adminProduct__info">{product.state}</p>
-                                  <Link
-                                    to={`/admin-details/${product.id}`}
-                                  >
-                                    <img className="adminProduct__edite" src={edite} alt="" />
-                                  </Link>
-                                </div>
-                              )}
-                            </div>
-                          ))
-                        )}
-                      </div>
-                    </>
-                  )}
-                </>
-              )}
-            </div>
+                                </Link>
+                              </div>
+                            )}
+                          </div>
+                        ))
+                      )}
+                    </div>
+                  </>
+                )}
+              </>
+            )}
           </div>
         </div>
-      </div>
       <Footer />
     </>
   );
