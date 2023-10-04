7cae303fa41925b3acc79e4b10cd39fd4354ebec
Neis Rosado <neisrosadom@gmail.com>
2023-10-03 23:33:58 -0500

    Fix edit price

--- src/pages/addProducts/AddProducts.jsx
+++ src/pages/addProducts/AddProducts.jsx
@@ -6,6 +6,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import './addProducts.scss'
+import Swal from 'sweetalert2';
 
 const AddProducts
     = () => {
