0e45b4e39df2eb42a92bbc13a9af2b3a3c341b41
ximena saldarriaga <xime-2208@hotmail.com>
2023-10-03 20:36:32 -0500

    create add products

--- src/pages/addProducts/AddProducts.jsx
+++ src/pages/addProducts/AddProducts.jsx
@@ -5,6 +5,7 @@ import Sidebar from "../../components/sidebar/Sidebar";
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
+import './addProducts.scss'
 
 const AddProducts
     = () => {
@@ -12,39 +13,38 @@ const AddProducts
         const navigate = useNavigate();
         const { register, handleSubmit } = useForm();
         const { error } = useSelector((store) => store.auth);
-      
-      
+
+
         const itemRegister = async (data) => {
-          try {
-            const imageFile = data.photoURL[0];
-            const avatar = await fileUpload(imageFile);
-            const newItem = {
-              ...data,
-              photoURL: avatar,
-            };
-            console.log(newItem);
-            dispatch(createAnItem(newItem));
-            Swal.fire("Excelente!", "Haz envíado los datos de tu producto!", "success");
-          } catch (error) {
-            Swal.fire("Oops!", "Hubo un error en el envío de los datos de tu producto", "error");
-          }
+            try {
+                const imageFile = data.photoURL[0];
+                const avatar = await fileUpload(imageFile);
+                const newItem = {
+                    ...data,
+                    photoURL: avatar,
+                };
+                console.log(newItem);
+                dispatch(createAnItem(newItem));
+                Swal.fire("Excelente!", "Haz envíado los datos de tu producto!", "success");
+            } catch (error) {
+                Swal.fire("Oops!", "Hubo un error en el envío de los datos de tu producto", "error");
+            }
         };
 
         return (
-            <div>
+            <div className='addProduct'>
                 <Header showSearchBar={false} />
                 <div className="products">
                     <div className="products__container">
                         <Sidebar />
-                        <div className="register flex flex-col items-center text-[14px] relative">
-                            <div className="container__login">
-
-                                
-                                <form className="w-[80%]" onSubmit={handleSubmit(itemRegister)}>
-                                    <div className="flex flex-col gap-6">
-                                        <div className="flex flex-col">
-                                            <label className="text-gray-400 text-[14px]  login__label">
-                                                Nombre del producto
+                        <div className="flex flex-col items-center text-[14px] relative">
+                            <h1 className='title fontGreen mb-10'>NUEVO PRODUCTO</h1>
+                            <form className="w-[80%]" onSubmit={handleSubmit(itemRegister)}>
+                                <div className="addProduct__dates">
+                                    <div className='addProduct__divForm'>
+                                        <div className="flex gap-4">
+                                            <label className=" fontGreen fontGreen">
+                                                Nombre:
                                             </label>
                                             <input
                                                 type="text"
@@ -52,9 +52,9 @@ const AddProducts
                                                 {...register("name")}
                                             />
                                         </div>
-                                        <div className="flex flex-col">
-                                            <label className="text-gray-400 text-[14px]  login__label">
-                                                Estado (nuevo o usado)
+                                        <div className="flex gap-4">
+                                            <label className=" fontGreen">
+                                                Estado:
                                             </label>
                                             <input
                                                 type="text"
@@ -62,17 +62,25 @@ const AddProducts
                                                 {...register("status")}
                                             />
                                         </div>
-                                        <div className="flex flex-col">
-                                            <label className="text-gray-400 login__label">Precio</label>
+                                        <div className="flex gap-4">
+                                            <label className=" fontGreen">Categoría:</label>
+                                            <input
+                                                type="text"
+                                                className="border-b border-gray-300 mb-2 outline-none "
+                                                {...register("category")}
+                                            />
+                                        </div>
+                                        <div className="flex gap-4">
+                                            <label className=" fontGreen">Precio:</label>
                                             <input
                                                 type="number"
                                                 className="border-b border-gray-300 mb-2 outline-none "
                                                 {...register("price")}
                                             />
                                         </div>
-                                        <div className="flex flex-col">
-                                            <label className="text-gray-400 text-[14px]  login__label">
-                                                Color
+                                        <div className="flex gap-4">
+                                            <label className="  fontGreen">
+                                                Color:
                                             </label>
                                             <input
                                                 type="text"
@@ -80,19 +88,10 @@ const AddProducts
                                                 {...register("color")}
                                             />
                                         </div>
-                                        <div className="flex flex-col">
-                                            <label className="text-gray-400 text-[14px]  login__label">
-                                                Descipción
-                                            </label>
-                                            <input
-                                                type="text"
-                                                className="border-b border-gray-300 mb-2 outline-none"
-                                                {...register("description")}
-                                            />
-                                        </div>
-                                        <div className="flex flex-col">
-                                            <label className="text-gray-400 text-[14px]  login__label">
-                                                Genero
+
+                                        <div className="flex gap-4">
+                                            <label className="  fontGreen">
+                                                Género:
                                             </label>
                                             <input
                                                 type="text"
@@ -100,17 +99,10 @@ const AddProducts
                                                 {...register("genre")}
                                             />
                                         </div>
-                                        <div className="flex flex-col">
-                                            <label className="text-gray-400 login__label">Precio</label>
-                                            <input
-                                                type="number"
-                                                className="border-b border-gray-300 mb-2 outline-none "
-                                                {...register("price")}
-                                            />
-                                        </div>
-                                        <div className="flex flex-col">
-                                            <label className="text-gray-400 text-[14px]  login__label">
-                                                Talla
+
+                                        <div className="flex gap-4">
+                                            <label className="  fontGreen">
+                                                Talla:
                                             </label>
                                             <input
                                                 type="text"
@@ -118,10 +110,23 @@ const AddProducts
                                                 {...register("size")}
                                             />
                                         </div>
+                                    </div>
 
-                                        <div className="flex flex-col">
-                                            <label className="text-gray-400 text-[14px]  login__label">
-                                                Descripción
+                                    <div className='addProduct__divForm'>
+                                        <div className="flex gap-4">
+                                            <label className="  fontGreen">
+                                                Descripción:
+                                            </label>
+                                            <input
+                                                type="text"
+                                                className="border-b border-gray-300 mb-2 outline-none"
+                                                {...register("description")}
+                                            />
+                                        </div>
+
+                                        <div className="flex gap-4">
+                                            <label className="  fontGreen">
+                                                Composición:
                                             </label>
                                             <input
                                                 type="text"
@@ -129,9 +134,9 @@ const AddProducts
                                                 {...register("text")}
                                             />
                                         </div>
-                                        <div className="flex flex-col">
-                                            <label className="text-gray-400 text-[14px]  login__label">
-                                                Titulo producto
+                                        <div className="flex gap-4">
+                                            <label className="  fontGreen">
+                                                Título:
                                             </label>
                                             <input
                                                 type="text"
@@ -139,54 +144,54 @@ const AddProducts
                                                 {...register("title")}
                                             />
                                         </div>
-                                        <div className="file flex flex-col">
-                                            <label className="text-gray-400 text-[14px]  login__label">
-                                                Foto frente
+                                        <div className="file flex gap-4">
+                                            <label className="  fontGreen">
+                                                Foto 1
                                             </label>
                                             <input
                                                 type="file"
-                                                className="file__box border-b border-gray-300 mb-2 outline-none"
+                                                className=""
                                                 {...register("photoURL")}
                                             />
                                         </div>
-                                        <div className="file flex flex-col">
-                                            <label className="text-gray-400 text-[14px]  login__label">
-                                                Foto atras
+                                        <div className="file flex gap-4">
+                                            <label className="  fontGreen">
+                                                Foto 2
                                             </label>
                                             <input
                                                 type="file"
-                                                className="file__box border-b border-gray-300 mb-2 outline-none"
+                                                className=""
                                                 {...register("photoURL")}
                                             />
                                         </div>
-                                        <div className="file flex flex-col">
-                                            <label className="text-gray-400 text-[14px]  login__label">
-                                                Foto de lado
+                                        <div className="file flex gap-4">
+                                            <label className="fontGreen">
+                                                Foto 3
                                             </label>
                                             <input
                                                 type="file"
-                                                className="file__box border-b border-gray-300 mb-2 outline-none"
+                                                className=""
                                                 {...register("photoURL")}
                                             />
                                         </div>
-                                        <div className="file">
-                                            <label className="text-gray-400 text-[14px]  login__label">
-                                                Foto material
+                                        <div className="file flex gap-4">
+                                            <label className="fontGreen">
+                                                Foto 4
                                             </label>
                                             <input
                                                 type="file"
-                                                className="file__box"
+                                                className=""
                                                 {...register("photoURL")}
                                             />
                                         </div>
                                     </div>
-                                    <div className="flex flex-col w-[100%] mt-8 items-center">
-                                        <button type="submit" className="button__login">
-                                            Enviar información
-                                        </button>
-                                    </div>
-                                </form>
-                            </div>
+                                </div>
+                                <div className="flex flex-col w-[100%] mt-8 items-center">
+                                    <button type="submit" className="button__login fontGreen">
+                                        Agregar producto
+                                    </button>
+                                </div>
+                            </form>
                         </div>
 
                     </div>
